1202c8e381155eeaf4f7925d1ffc5eb1a982632f
Astarixi <romain.amuat@gmail.com>
2018-04-16 00:25:51 -0400

    Adding more exception catching in the view to make the server more stable

--- src/view/ServiceManager.cpp
+++ src/view/ServiceManager.cpp
@@ -37,44 +37,58 @@ void ServiceManager::setupRoutes() {
 }
 
 void ServiceManager::post_new_player(const Rest::Request& request, Http::ResponseWriter response) {
-    std::cout << "ServiceManager.cpp - Analysing request for creating new player" << std::endl; 
+    try {
+        std::cout << "ServiceManager.cpp - Analysing request for creating new player" << std::endl; 
 
-    Player ret = player_controler->create();
-    std::string out = ret.to_json_string();
+        Player ret = player_controler->create();
+        std::string out = ret.to_json_string();
 
-    std::cout << "ServiceManager.cpp - Sending answer for creating new player" << std::endl; 
-    response.send(Http::Code::Ok, out);       
-    std::cout << "ServiceManager.cpp - Answer sent for creating new player\n" << std::endl;
+        std::cout << "ServiceManager.cpp - Sending answer for creating new player" << std::endl; 
+        response.send(Http::Code::Ok, out);       
+        std::cout << "ServiceManager.cpp - Answer sent for creating new player\n" << std::endl;
+    catch(...) {
+        std::cout << "ServiceManager.cpp - exception occured in the view in post_new_player" << std::endl;
+    }
 }
 
 void ServiceManager::get_all_players(const Rest::Request& request, Http::ResponseWriter response) {
-    std::cout << "ServiceManager.cpp - Analysing request for getting all players" << std::endl; 
-    
-    std::vector<Player> ret = player_controler->get();
-    
-    Json::Value val;
-    for (int i=0; i < ret.size(); i++) {
-        val.append(ret[i].to_json());
-    }
-    std::string r = val.toStyledString();
+    try{
+        std::cout << "ServiceManager.cpp - Analysing request for getting all players" << std::endl; 
+        
+        std::vector<Player> ret = player_controler->get();
+        
+        Json::Value val;
+        for (int i=0; i < ret.size(); i++) {
+            val.append(ret[i].to_json());
+        }
+        std::string r = val.toStyledString();
 
-    std::cout << "ServiceManager.cpp - Sending answer for getting all players" << std::endl; 
-    response.send(Http::Code::Ok, r);       
-    std::cout << "ServiceManager.cpp - Answer sent for getting all player\n" << std::endl;
+        std::cout << "ServiceManager.cpp - Sending answer for getting all players" << std::endl; 
+        response.send(Http::Code::Ok, r);       
+       std::cout << "ServiceManager.cpp - Answer sent for getting all player\n" << std::endl;
+    }
+    catch(...) {
+        std::cout << "ServiceManager.cpp - exception occured in the view in get_all_players" << std::endl;
+    }
 }
 
 void ServiceManager::get_player_from_id(const Rest::Request& request, Http::ResponseWriter response) {
-    std::cout << "ServiceManager.cpp - Analysing request for getting player from id" << std::endl; 
-    
-    auto id = request.param(":id").as<int>();
-    
-    Player ret = player_controler->read_from_id(id);
-    std::string out = ret.to_json_string();
+    try {
+        std::cout << "ServiceManager.cpp - Analysing request for getting player from id" << std::endl; 
+        
+        auto id = request.param(":id").as<int>();
+        
+        Player ret = player_controler->read_from_id(id);
+        std::string out = ret.to_json_string();
 
-    std::cout << "ServiceManager.cpp - Sending answer for getting player from id" << std::endl; 
-    response.send(Http::Code::Ok, out);  
-    
-    std::cout << "ServiceManager.cpp - Answer sent for getting player from id\n" << std::endl; 
+        std::cout << "ServiceManager.cpp - Sending answer for getting player from id" << std::endl; 
+        response.send(Http::Code::Ok, out);  
+        
+        std::cout << "ServiceManager.cpp - Answer sent for getting player from id\n" << std::endl; 
+    }
+    catch(...) {
+        std::cout << "ServiceManager.cpp - exception occured in the view in get_player_from_id" << std::endl;
+    }
 }
 
 void ServiceManager::delete_player_from_id(const Rest::Request& request, Http::ResponseWriter response) {
